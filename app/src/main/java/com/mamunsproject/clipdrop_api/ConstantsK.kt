2b5bde90cf12bce953db38a4993db9adedf55d01
Abdullah Al Mamun <aamamun25000@gmail.com>
2023-09-06 09:40:16 +0600

    MVVM

---
+++ app/src/main/java/com/mamunsproject/clipdrop_api/ConstantsK.kt
@@ -0,0 +1,6 @@
+package com.mamunsproject.clipdrop_api
+
+object ConstantsK {
+    const val CLIP_USER_ID = "UlHyuGLCW9YuOgMRumSIvbY0JXn2"
+    const val CLIP_API_KEY = "834fa36f0ff603b4d731283537869413a8a0c0984f32682d098e9646d8f068d434e1116d0385006e0db9c8fbb33ffa56"
+}
